0f5bea46798838fd1c05dde9070e6004f8b040b2
Humberto <h.maury@uniandes.edu.co>
2024-10-09 21:09:34 -0500

    linter

--- src/app/incident/incident.component.ts
+++ src/app/incident/incident.component.ts
@@ -1,65 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-incident',
   templateUrl: './incident.component.html',
   styleUrls: ['./incident.component.css']
 })
-export class IncidentComponent implements OnInit {
+export class IncidentComponent {
 
-  constructor() { }
+  title = "Incidentes";
 
-  ngOnInit() {
-  }
-
-  function1 () {
-    console.log('')
-  }
-
-  function2 () {
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-  }
-
-  function3 () {
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-    console.log('')
-  }
-
-  function4 () {
-    console.log('')
-  }
-
-  function5 () {
-    console.log('')
-  }
-
-  function6 () {
-    console.log('')
-  }
 }
