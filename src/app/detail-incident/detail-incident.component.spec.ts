7705e180389d2704d93be773db819bfba47b4e7e
mamunozb <ma.munozb1@uniandes.edu.co>
2024-11-11 17:54:34 -0500

    feature/detalle-incidente1

---
+++ src/app/detail-incident/detail-incident.component.spec.ts
@@ -0,0 +1,308 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DetailIncidentComponent } from './detail-incident.component';
+import { IncidentDetailService } from './detail-incident.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgxLoadingModule } from '@dchtools/ngx-loading-v18';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { Type } from '../models/type';
+import { of } from 'rxjs';
+
+describe('IncidentDetailComponent', () => {
+  let component: DetailIncidentComponent;
+  let fixture: ComponentFixture<DetailIncidentComponent>;
+  let mockDetailIncidentService: jasmine.SpyObj<IncidentDetailService>;
+  let router: Router;
+
+  const mockRouter = {
+    navigate: jasmine.createSpy('navigate') // Espía correctamente el router
+  };
+
+
+  const mockActivatedRoute = {
+    snapshot: {
+      paramMap: {
+        get: (key: string) => key === 'id' ? '1' : null  // Simula que devuelve "1" para "id"
+      }
+    }
+  };
+
+  beforeEach(async () => {
+    mockDetailIncidentService = jasmine.createSpyObj('IncidentService', ['getById', 'getAllClients', 'getAllUsers']);
+    mockDetailIncidentService.getById.and.returnValue(of({ title: 'Test Incident',type: Type.Other, description: 'Test Description', clientid: '123', iduser: 456, state: 1, agentid: '1', serviceid: '1', userid: '1' }));
+    mockDetailIncidentService.getAllClients.and.returnValue(of([{ id: '456', name: 'client1' }, { id: '890', name: 'client2' }]));
+    mockDetailIncidentService.getAllUsers.and.returnValue(of([{ id_number: '123', username: 'user1' }, { id_number: '456', username: 'user2' }]));
+
+    await TestBed.configureTestingModule({
+      declarations: [DetailIncidentComponent],
+      imports: [
+        CommonModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatCheckboxModule,
+        MatButtonModule,
+        MatDatepickerModule,
+        ReactiveFormsModule,
+        NgxLoadingModule.forRoot({}),
+        BrowserAnimationsModule,
+        MatAutocompleteModule,
+        MatIconModule,
+        MatSlideToggleModule,
+      ],
+      providers: [
+        { provide: IncidentDetailService, useValue: mockDetailIncidentService }, // Solo un proveedor para IncidentService
+        { provide: Router, useValue: mockRouter }, // Espía para el router
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        provideNativeDateAdapter()
+      ]
+    }).compileComponents();
+
+
+
+    fixture = TestBed.createComponent(DetailIncidentComponent);
+    component = fixture.componentInstance;
+    router = TestBed.inject(Router);
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should get the incidentId from route parameters', () => {
+    expect(component.incidentId).toBe('1');
+  });
+
+  it('should navigate to login', () => {
+    component.goBack(); // Llama al método que se va a probar
+    expect(router.navigate).toHaveBeenCalledWith(['/', 'app', 'incident']); // Verifica que se llame a navigate con la ruta esperada
+  });
+
+  it('should have a valid form when fields are filled correctly', () => {
+    component.incidentDetailForm.controls['title'].setValue('Incidente de prueba');
+    component.incidentDetailForm.controls['type'].setValue(1);
+    component.incidentDetailForm.controls['description'].setValue('Descripción del incidente');
+    component.incidentDetailForm.controls['clientid'].setValue('Cliente1');
+    component.incidentDetailForm.controls['iduser'].setValue('1');
+
+    expect(component.incidentDetailForm.valid).toBeTrue();  // El formulario debe ser válido
+  });
+
+  it('should filter users by id_number or username', () => {
+    component.allUsers = [
+      { id_number: '123', username: 'Usuario1' },
+      { id_number: '456', username: 'Usuario2' }
+    ];
+
+    component.onSearch('123');
+    expect(component.filteredUsers.length).toBe(1);
+    expect(component.filteredUsers[0].id_number).toBe('123');
+
+    component.onSearch('usuario2');
+    expect(component.filteredUsers.length).toBe(1);
+    expect(component.filteredUsers[0].username).toBe('Usuario2');
+  });
+
+  it('should update userid when a user is selected', () => {
+    // Crear un evento simulado con el formato esperado
+    const mockEvent = {
+      option: {
+        value: {
+          id_number: '12345'  // Valor simulado del id de usuario
+        }
+      }
+    };
+    // Llamar a la función con el evento simulado
+    component.onUserSelected(mockEvent);
+    // Verificar que el valor de userid se haya actualizado correctamente
+    expect(component.userid).toBe('12345');
+  });
+
+  it('should return formatted string when a valid user is provided', () => {
+    const mockUser = { id_number: 12345, username: 'JohnDoe' };
+
+    // Llamar a displayUser con un usuario válido
+    const result = component.displayUser(mockUser);
+
+    // Verificar que el resultado sea el formato esperado
+    expect(result).toBe('12345 - JohnDoe');
+  });
+
+  it('should return an empty string when no user is provided', () => {
+    // Llamar a displayUser con un valor nulo
+    const result = component.displayUser(null);
+
+    // Verificar que el resultado sea una cadena vacía
+    expect(result).toBe('');
+  });
+
+  it('should return an empty string when user is undefined', () => {
+    // Llamar a displayUser con un valor undefined
+    const result = component.displayUser(undefined);
+
+    // Verificar que el resultado sea una cadena vacía
+    expect(result).toBe('');
+  });
+
+  it('should set selectedFile when a file is selected', () => {
+    // Crear un archivo simulado
+    const mockFile = new File(['file content'], 'test-file.txt', { type: 'text/plain' });
+
+    // Simular el evento de selección de archivo
+    const event = {
+      target: {
+        files: [mockFile]  // Simular que se seleccionó un archivo
+      }
+    };
+
+    // Llamar a la función onFileSelected con el evento simulado
+    component.onFileSelected(event);
+
+    // Verificar que selectedFile se haya establecido correctamente
+    expect(component.selectedFile).toBe(mockFile);
+  });
+
+  it('should not set selectedFile when no file is selected', () => {
+    // Simular el evento sin ningún archivo seleccionado
+    const event = {
+      target: {
+        files: []  // Ningún archivo seleccionado
+      }
+    };
+
+    // Llamar a la función onFileSelected con el evento simulado
+    component.onFileSelected(event);
+
+    // Verificar que selectedFile sea null cuando no se selecciona ningún archivo
+    expect(component.selectedFile).toBeNull();
+  });
+
+  it('should enable phone toggle', () => {
+    component.onToggleChange('phone');
+    expect(component.isPhoneEnabled).toBe(true);
+    expect(component.isMailEnabled).toBe(false);
+    expect(component.serviceId).toBe('1');
+  });
+
+  it('should enable mail toggle', () => {
+    component.onToggleChange('mail');
+    expect(component.isMailEnabled).toBe(true);
+    expect(component.isPhoneEnabled).toBe(false);
+    expect(component.serviceId).toBe('2');
+  });
+
+  it('should disable smartphone toggle if phone is enabled', () => {
+    component.isPhoneEnabled = true;
+    component.isMailEnabled = false;
+    component.updateToggleStates();
+    expect(component.isSmartphoneEnabled).toBe(false);
+  });
+
+  it('should disable smartphone toggle if mail is enabled', () => {
+    component.isPhoneEnabled = false;
+    component.isMailEnabled = true;
+    component.updateToggleStates();
+    expect(component.isSmartphoneEnabled).toBe(false);
+  });
+
+  it('should clear the required validator for iduser when user type is 1', () => {
+    component.user = { type: 1 }; // Tipo de usuario 1
+    const clientidControl = component.incidentDetailForm.get('iduser');
+    clientidControl?.setValidators(Validators.required); // Añadir validator para probar que se remueva
+
+    component.setUserValidator();
+
+    expect(clientidControl?.hasValidator(Validators.required)).toBeFalse();
+  });
+
+  it('should clear the required validator for iduser when user type is 1', () => {
+    // Asigna un tipo de usuario 1 para que se ejecute el bloque else
+    component.user = { type: 1 };
+
+    // Configura el validador 'required' inicialmente
+    const clientidControl = component.incidentDetailForm.get('iduser');
+    clientidControl?.setValidators(Validators.required);
+    clientidControl?.updateValueAndValidity();
+
+    // Llama a setUserValidator para activar la lógica del else
+    component.setUserValidator();
+
+    // Verifica que el validador requerido se haya eliminado
+    expect(clientidControl?.hasValidator(Validators.required)).toBeFalse();
+  });
+  it('should clear the required validator for clientid when user type is 3', () => {
+    // Asigna un tipo de usuario 3 para que se ejecute el bloque else
+    component.user = { type: 3 };
+
+    // Configura el validador 'required' inicialmente
+    const clientidControl = component.incidentDetailForm.get('clientid');
+    clientidControl?.setValidators(Validators.required);
+    clientidControl?.updateValueAndValidity();
+
+    // Llama a setClientValidator para activar la lógica del else
+    component.setClientValidator();
+
+    // Verifica que el validador requerido se haya eliminado
+    expect(clientidControl?.hasValidator(Validators.required)).toBeFalse();
+  });
+
+  it('should clear the required validator for clientid when user type is 3', () => {
+    component.user = { type: 3 }; // Tipo de usuario 3
+    const clientidControl = component.incidentDetailForm.get('clientid');
+    clientidControl?.setValidators(Validators.required); // Añadir validator para probar que se remueva
+
+    component.setClientValidator();
+
+    expect(clientidControl?.hasValidator(Validators.required)).toBeFalse();
+  });
+
+  it('should fetch and set users correctly in getClients()', () => {
+    const mockAllClient = [{ id: '456', name: 'client1' }, { id: '890', name: 'client2' }];
+    mockDetailIncidentService.getAllClients.and.returnValue(of(mockAllClient));
+
+    component.getClients();
+
+    expect(mockDetailIncidentService.getAllClients).toHaveBeenCalled();
+    expect(component.allClients).toEqual(mockAllClient);
+    expect(component.filteredClients).toEqual(mockAllClient);
+  });
+
+  it('should fetch and set users correctly in getusers()', () => {
+    const mockAllUser = [{ id_number: '123', username: 'user1' }, { id_number: '456', username: 'user2' }];
+    mockDetailIncidentService.getAllUsers.and.returnValue(of(mockAllUser));
+
+    component.getUsers();
+
+    expect(mockDetailIncidentService.getAllClients).toHaveBeenCalled();
+    expect(component.allUsers).toEqual(mockAllUser);
+    expect(component.filteredUsers).toEqual(mockAllUser);
+  });
+
+  it('should call getById with the correct id', () => {
+    const mockId = '123';
+    const mockData = { title: 'Test Incident',type: Type.Other, description: 'Test Description', clientid: '123', iduser: 456, state: 1, agentid: '1', serviceid: '1', userid: '1' };
+    mockDetailIncidentService.getById.and.returnValue(of(mockData));
+
+    component.getIncidentDetail(mockId);
+
+    expect(mockDetailIncidentService.getById).toHaveBeenCalledWith(mockId);
+    expect(component.isPhoneEnabled).toBe(true);
+    expect(component.isMailEnabled).toBe(false);
+    expect(component.isSmartphoneEnabled).toBe(false);
+    expect(component.incidentDetailForm.value.clientid).toEqual(mockData.clientid);
+  });
+
+});
---
+++ src/app/detail-incident/detail-incident.component.ts
@@ -0,0 +1,211 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Component, OnInit  } from '@angular/core';
+import { IncidentDetailService } from './detail-incident.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Type } from '../models/type';
+import { User } from '../models/user';
+import { Incident } from '../models/incident';
+import { LayoutService } from '../layout/layout.service';
+import { State } from '../models/state';
+
+@Component({
+  selector: 'app-incident',
+  templateUrl: './detail-incident.component.html',
+  styleUrl: './detail-incident.component.css'
+})
+export class DetailIncidentComponent implements OnInit {
+  state = 'Detail';
+  IncidentType = Type;
+  incidentDetailForm = new FormGroup({
+    title: new FormControl('', [ Validators.maxLength(50)]),
+    type: new FormControl<Type | null>(0),
+    description: new FormControl(''),
+    clientid: new FormControl('', [Validators.required]),
+    iduser: new FormControl('', [Validators.required]),
+    state: new FormControl(''),
+  });
+  loading = false;
+  done = false;
+  isPhoneEnabled = true;
+  isMailEnabled = false;
+  isSmartphoneEnabled = false;
+  serviceId = "1";
+  selectedFile: File | null = null;
+  filteredUsers: any[] = [];
+  allUsers: any[] = [];
+  userid: string | null = null;
+  filteredClients: any[] = [];
+  allClients: any[] = [];
+  user: User | undefined = undefined;
+  incidentDetail: Incident | undefined = undefined;
+  incidentId: string | null = null;
+
+  constructor(private route: ActivatedRoute, private router: Router, private incidentService: IncidentDetailService,
+    private layoutService: LayoutService ) {
+    this.user = layoutService.getUser();
+    this.setClientValidator();
+    this.setUserValidator();
+  }
+  ngOnInit(): void {
+    this.incidentId = this.route.snapshot.paramMap.get('id');
+    this.getClients();
+
+    if (this.incidentId) {
+      this.getUsers();
+      this.getIncidentDetail(this.incidentId);
+    }
+
+    this.incidentDetailForm.get('iduser')?.valueChanges.subscribe(value => {
+      this.onSearch(value ?? '');
+    });
+
+  }
+
+  goBack() {
+    this.router.navigate(['/', 'app', 'incident']);
+  }
+
+  getUserData() {
+      return this.incidentService.user_token();
+  }
+
+  getIncidentStateString(state: State | undefined): string {
+    if (state === undefined) return '';
+    return State[state];
+  }
+
+  isValid() {
+    return this.incidentDetailForm.valid;
+  }
+
+  getIncidentDetail(id: string): void {
+    this.incidentService.getById(id).subscribe(
+      (data) => {
+        this.incidentDetail = data;
+        if( Number(this.incidentDetail?.serviceid) === 1 ){
+          this.isPhoneEnabled = true;
+          this.isMailEnabled = false;
+          this.isSmartphoneEnabled = false;
+        }
+        if( Number(this.incidentDetail?.serviceid) === 2 ){
+          this.isPhoneEnabled = false;
+          this.isMailEnabled = true;
+          this.isSmartphoneEnabled = false;
+        }
+        if( Number(this.incidentDetail?.serviceid) === 3 ){
+          this.isPhoneEnabled = false;
+          this.isMailEnabled = false;
+          this.isSmartphoneEnabled = true;
+        }
+        const defaultUser = this.filteredUsers.find(user => user.id_number === Number(this.incidentDetail?.userid));
+        this.incidentDetailForm.patchValue({
+          clientid: this.incidentDetail.clientid,
+          iduser: defaultUser,
+          type: this.incidentDetail.type,
+        });
+      },
+      (error) => {
+        console.error('Error al obtener el detalle del incidente:', error);
+      }
+    );
+  }
+
+  onToggleChange(toggleType: string) {
+    // Resetear los valores
+    this.isPhoneEnabled = false;
+    this.isMailEnabled = false;
+
+    // Activar el toggle correspondiente
+    if (toggleType === 'phone') {
+      this.isPhoneEnabled = true;
+      this.isMailEnabled = false;
+      this.serviceId = "1"; // Valor para el toggle 'phone'
+    } else if (toggleType === 'mail') {
+      this.isMailEnabled = true;
+      this.isPhoneEnabled = false;
+      this.serviceId = "2"; // Valor para el toggle 'mail'
+    }
+
+    // Deshabilitar el otro toggle
+    this.updateToggleStates();
+  }
+
+  updateToggleStates() {
+    // Si hay un toggle activado, deshabilitar los otros
+    if (this.isPhoneEnabled || this.isMailEnabled) {
+      this.isSmartphoneEnabled = false; // Si es necesario, puedes deshabilitar este también
+    } else {
+      this.isSmartphoneEnabled = true; // Si ambos están desactivados
+    }
+  }
+  onFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+    if (file) {
+      this.selectedFile = file;
+      console.log('Archivo seleccionado:', file);
+    }
+  }
+
+  displayUser(user: any): string {
+      return user ? `${user.id_number} - ${user.username}` : '';
+  }
+
+  getUsers(): void {
+    this.incidentService.getAllUsers().subscribe((users) => {
+      this.allUsers = users;  // Guardamos todos los usuarios
+      this.filteredUsers = users;  // Inicialmente mostramos todos
+    });
+  }
+
+  getClients(): void {
+    this.incidentService.getAllClients().subscribe((clients) => {
+      this.allClients = clients;  // Guardamos todos los usuarios
+      this.filteredClients = clients;  // Inicialmente mostramos todos
+    });
+  }
+
+  onSearch(searchTerm: any): void {
+    if (typeof searchTerm === 'string' && searchTerm) {
+      // Filtrar usuarios por id_number o cualquier otro criterio
+      this.filteredUsers = this.allUsers.filter(user =>
+        user.id_number.toString().includes(searchTerm)  ||
+        user.username.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    } else {
+      // Si no hay término de búsqueda, mostrar todos los usuarios
+      this.filteredUsers = this.allUsers;
+    }
+  }
+
+  onUserSelected(event: any): void {
+      this.userid = event.option.value.id_number;
+  }
+
+  setClientValidator() {
+    const clientidControl = this.incidentDetailForm.get('clientid');
+
+    if (this.user?.type !== 3) {
+      clientidControl?.setValidators(Validators.required);
+    } else {
+      clientidControl?.clearValidators();
+    }
+
+    // Asegúrate de que Angular vuelva a validar este campo
+    clientidControl?.updateValueAndValidity();
+  }
+
+  setUserValidator() {
+    const useridControl = this.incidentDetailForm.get('iduser');
+
+    if (this.user?.type !== 1) {
+      useridControl?.setValidators(Validators.required);
+    } else {
+      useridControl?.clearValidators();
+    }
+
+    // Asegúrate de que Angular vuelva a validar este campo
+    useridControl?.updateValueAndValidity();
+  }
+
+}
