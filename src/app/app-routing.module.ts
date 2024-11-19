841e54afd2ca20306842037f2215a29741626be3
Andres Camilo Cardenas <liderdesarrollo@tht.company>
2024-11-18 21:12:09 -0500

    Feature predictive Indicators

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,6 +24,7 @@ const routes: Routes = [
       { path: 'home',  loadChildren: () => import('./home/home.module').then(m => m.HomeModule), canActivate: [AuthGuard] },
       { path: 'incident', loadChildren: () => import('./incident/incident.module').then(m => m.IncidentModule), canActivate: [AuthGuard]},
       { path: 'control-board', loadChildren: () => import('./control-board/control-board.module').then(m => m.ControlBoardModule) , canActivate: [AuthGuard]},
+      { path: 'control-board-predictive', loadChildren: () => import('./control-board-predicitve/control-board-predicitve.module').then(m => m.ControlBoardPredicitveModule) , canActivate: [AuthGuard]},
       { path: 'create-incident', loadChildren: () => import('./create-incident/create-incident.module').then(m => m.CreateIncidentModule) , canActivate: [AuthGuard]},
       { path: 'detail-incident/:id', loadChildren: () => import('./detail-incident/detail-incident.module').then(m => m.DetailIncidentModule) , canActivate: [AuthGuard]},
       {
